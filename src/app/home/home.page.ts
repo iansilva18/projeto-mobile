56935b05cbc2c2d6917e906f326e2e46db1553ef
ian silva <106530553+iansilva18@users.noreply.github.com>
2022-08-26 10:18:21 -0300

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  entrada1 : string
+  entrada2 : string
+  mensagen : string ='ola mundo'
 
   constructor() {}
+
+  escreva(){
+    var num1 = parseFloat(this.entrada1)
+    var num2 = parseFloat(this.entrada2)
+    var soma = num1 + num2
+    this.mensagen = soma.toString()
+  }
 
 }
